1b3b6d2144dcdfb1fee6f60ceacab5d1edb8eea4
elio.Jung <elio.Jung@obigo.com>
2025-05-30 10:33:54 +0900

    class: 1장.클래스와 데이터 완료

---
+++ README.md
@@ -0,0 +1,12 @@
+# java-basic
+
+## 섹션2. 클래스와 데이터
+
+- 생성자를 통해 변수에 객체를 할당하는 과정
+- 실제로 변수에는 인스턴스가 아닌 참조값이 들어간다는 걸 명심
+
+```declarative
+Student student = new Student(); // 1. Student 객체 생성
+Student student = x001; // 2. new Student()의 결과로 x001 참조값 반환
+student1 = x001; // 3. 최종 결과
+```
--- src/class1/ClassStart2.java
+++ src/class1/ClassStart2.java
@@ -1,4 +1,15 @@
 package class1;
 
+// 배열로 관리하면 코드가 간단해지지만, 학생이라는 개념을 하나로 묶지 않아 데이터가 분산된 형태라 데이터를 변경할 때 오류가 날 위험이 높다.
+
 public class ClassStart2 {
+    public static void main(String[] args) {
+        String[] studentNames = {"학생1", "학생2", "학생3"};
+        int[] studentAges = {15, 16, 17};
+        int[] studentGrades = {80, 90, 100};
+
+        for (int i = 0; i < studentNames.length; i++) {
+            System.out.println("이름:" + studentNames[i] + " 나이:" + studentAges[i] + " 성적:" + studentGrades[i]);
+        }
+    }
 }
