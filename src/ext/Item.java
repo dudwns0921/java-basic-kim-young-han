db044e2896e30d8a6091d142f3b24668bb140b4e
elio.Jung <elio.Jung@obigo.com>
2025-06-20 12:28:18 +0900

    class: 9장 상속

---
+++ src/ext/Item.java
@@ -0,0 +1,20 @@
+package ext;
+
+public class Item {
+
+  String name;
+  int price;
+
+  public Item(String name, int price) {
+    this.name = name;
+    this.price = price;
+  }
+
+  void print() {
+    System.out.println("이름: " + name + ", 가격: " + price);
+  }
+
+  int getPrice() {
+    return price;
+  }
+}
---
+++ src/ext/Movie.java
@@ -0,0 +1,21 @@
+package ext;
+
+public class Movie extends Item {
+
+  String director;
+  String actor;
+
+  public Movie(String name, int price, String director, String actor) {
+    super(name, price);
+    this.director = director;
+    this.actor = actor;
+  }
+
+  @Override
+  void print() {
+    super.print();
+    System.out.println("감독: " + director + ", 배우: " + actor);
+  }
+
+
+}
